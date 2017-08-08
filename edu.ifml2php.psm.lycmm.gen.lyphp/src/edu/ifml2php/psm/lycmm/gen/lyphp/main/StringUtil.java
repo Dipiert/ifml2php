e2cb72041e73ff453db4877ad32e42ea230b2abc
dipiert <dipiert@gmail.com>
2017-08-08 20:57:35 -0300

    Done Anchors Generation

--- edu.ifml2php.psm.lycmm.gen.lyphp/src/edu/ifml2php/psm/lycmm/gen/lyphp/main/StringUtil.java
+++ edu.ifml2php.psm.lycmm.gen.lyphp/src/edu/ifml2php/psm/lycmm/gen/lyphp/main/StringUtil.java
@@ -10,4 +10,16 @@ public class StringUtil {
 		return "";
 	}
 	
+	public String getPrefix(String str){
+		char[] chars = str.toCharArray();
+		for(int i = chars.length - 1; i > 0; i--)
+			if(chars[i] == Character.toUpperCase(chars[i]))
+				return str.substring(0,i);		
+		return "";
+	}
+	
+	public String separatePrefixSuffix(String str){
+		return getPrefix(str) + " " + getSuffix(str);
+	}	
+	
 }
