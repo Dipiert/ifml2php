c4f51006be033d31cf861065e97d0b9d7400d767
dipiert <dipiert@gmail.com>
2017-08-08 17:38:31 -0300

    Add StringUtil (helper and bean)

---
+++ edu.ifml2php.psm.lycmm.gen.lyphp/src/edu/ifml2php/psm/lycmm/gen/lyphp/main/StringUtil.java
@@ -0,0 +1,13 @@
+package edu.ifml2php.psm.lycmm.gen.lyphp.main;
+
+public class StringUtil {
+
+	public String getSuffix(String str){
+		char[] chars = str.toCharArray();
+		for(int i = chars.length - 1; i > 0; i--)
+			if(chars[i] == Character.toUpperCase(chars[i]))
+				return str.substring(i).toLowerCase();		
+		return "";
+	}
+	
+}
