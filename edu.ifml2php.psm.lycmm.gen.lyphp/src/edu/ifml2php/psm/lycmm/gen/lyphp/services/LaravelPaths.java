111ec17799c7d2053c82434b7ada960886bd5083
dipiert <dipiert@gmail.com>
2017-08-11 18:59:53 -0300

    Refactor Acceleo Packages

---
+++ edu.ifml2php.psm.lycmm.gen.lyphp/src/edu/ifml2php/psm/lycmm/gen/lyphp/services/LaravelPaths.java
@@ -0,0 +1,16 @@
+package edu.ifml2php.psm.lycmm.gen.lyphp.services;
+
+public class LaravelPaths {
+   String baseDir;// = "/var/www/html/";
+
+   public String getBaseDir() {
+      return baseDir;
+   }
+
+   public void setBaseDir(String baseDir) {
+      this.baseDir = baseDir;
+   }
+
+   
+   
+}
---
+++ edu.ifml2php.psm.lycmm.gen.lyphp/src/edu/ifml2php/psm/lycmm/gen/lyphp/services/StringUtil.java
@@ -0,0 +1,30 @@
+package edu.ifml2php.psm.lycmm.gen.lyphp.services;
+
+public class StringUtil {
+	char[] chars;
+	
+	public String getSuffix(String str) {
+		chars = str.toCharArray();
+		for(int i = chars.length - 1; i > 0; i--)
+			if(chars[i] == Character.toUpperCase(chars[i]))
+				return capitalize(str.substring(i));		
+		return "";
+	}
+	
+	public String getPrefix(String str) {
+		chars = str.toCharArray();
+		for(int i = chars.length - 1; i > 0; i--)
+			if(chars[i] == Character.toUpperCase(chars[i]))
+				return str.substring(0,i);		
+		return "";
+	}
+	
+	public String separatePrefixSuffix(String str) {
+		return getPrefix(str) + " " + getSuffix(str);
+	}	
+	
+	private String capitalize(String str) {
+		return str.substring(0, 1).toUpperCase() + str.substring(1).toLowerCase(); 
+	}
+	
+}
