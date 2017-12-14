28551b1192f6342b95751f46aa909e01187f25c2
dipiert <dipiert@gmail.com>
2017-12-14 19:11:44 -0300

    Add FormTest & Refactor

--- selenium.ifml2php/Tests.java
+++ selenium.ifml2php/Tests.java
@@ -1,5 +1,8 @@
 package selenium.ifml2php;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import org.testng.AssertJUnit;
@@ -9,20 +12,24 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 
 public class Tests {
-	static FirefoxDriver driver;
-	static String URL;
-	static String[] pagesTitles;
-	static String[] anchorsLinks;
-	static String[] pagesWithImages;
-	
+	private static FirefoxDriver driver;
+	private static String URL;
+	private static String[] pagesTitles, pagesWithImages;
+	private static Map<String, String[]> anchorsLinks;
+    private static Map<String, String> pagesWithForms;
+    private static String addFormMovie, updateFormMovie, deleteFormMovie, mainMenuMovie;
+    private static String _addFormMovie, _updateFormMovie, _deleteFormMovie, _mainMenuMovie;
+    
 	@BeforeClass
 	private static void setUp() {
 		System.setProperty("webdriver.gecko.driver", "/home/dam/Descargas/eclipse/luna/geckodriver");
 		driver = new FirefoxDriver();
-		URL = makeURL();
-		pagesTitles = new String[]{"MainMenuMovie", "AddFormMovie", "UpdateFormMovie", "DeleteFormMovie"};
-		anchorsLinks = new String[]{"Main Menu Movie", "Add Form Movie", "Update Form Movie", "Delete Form Movie"};
-		pagesWithImages = new String[]{"MainMenuMovie"};
+		makeURL();
+		getStrings();
+		getPagesTitles();		
+		getAnchorsLinks();
+		getPagesWithImages();
+		getPagesWithForms();		
 	}
 	
 	@AfterClass
@@ -30,11 +37,50 @@ public class Tests {
 		driver.close();
 	}
 	
-	private static String makeURL() {
+	private static void getStrings() {
+		addFormMovie = "Add Form Movie";
+		_addFormMovie = addFormMovie.replaceAll("\\s+", "");
+	    updateFormMovie = "Update Form Movie";
+	    _updateFormMovie = updateFormMovie.replaceAll("\\s+", "");
+	    deleteFormMovie = "Delete Form Movie";
+	    _deleteFormMovie = deleteFormMovie.replaceAll("\\s+", "");
+	    mainMenuMovie = "Main Menu Movie";
+	    _mainMenuMovie = mainMenuMovie.replaceAll("\\s+", "");
+	}
+
+	private static void getPagesTitles() {
+		pagesTitles = new String[]{_mainMenuMovie, _addFormMovie, _updateFormMovie, _deleteFormMovie};
+	}
+	
+	private static void getAnchorsLinks() {
+		anchorsLinks = new HashMap<String, String[]>();
+		String[] anchorsMainMenu = {addFormMovie, updateFormMovie, deleteFormMovie};
+		String[] anchorsAddForm = {mainMenuMovie , updateFormMovie, deleteFormMovie};
+		String[] anchorsUpdateForm = {mainMenuMovie , addFormMovie, deleteFormMovie};
+		String[] anchorsDeleteForm = {mainMenuMovie , addFormMovie, updateFormMovie};		
+		String[] titles = {_mainMenuMovie,_addFormMovie,_updateFormMovie,_deleteFormMovie};
+		String[][] anchors = {anchorsMainMenu, anchorsAddForm, anchorsUpdateForm, anchorsDeleteForm};
+		for(int i = 0; i < titles.length; i++) {		
+			anchorsLinks.put(titles[i], anchors[i]);
+		}
+	}
+	
+	private static void getPagesWithForms() {
+		pagesWithForms = new HashMap<String, String>();
+		pagesWithForms.put(_addFormMovie, _addFormMovie);
+		pagesWithForms.put(_updateFormMovie, _updateFormMovie);
+		pagesWithForms.put(_deleteFormMovie, _deleteFormMovie);
+	}
+	
+	private static void getPagesWithImages() {
+		pagesWithImages = new String[]{_mainMenuMovie};
+	}
+	
+	private static void makeURL() {
 		String server= "localhost";
 		String project = "laravel5.4.15";
 		String controller = "movie";
-		return "http://" + server + "/" + project + "/public/" + controller + "/";
+		URL = "http://" + server + "/" + project + "/public/" + controller + "/";
 	}
 	
 	@Test(enabled = true)	
@@ -47,16 +93,18 @@ public class Tests {
 	
 	@Test(enabled = true)
 	public static void testAPageAnchors() {
-		for(String anchorLink : anchorsLinks){
-			for(String title : anchorsLinks){
-				String _title = title.replaceAll("\\s+","");
-				driver.get(URL + _title);
-				if (! _title.equals(driver.getTitle())) {
-					WebElement el = driver.findElementByLinkText(title);
-					AssertJUnit.assertNotNull(el);
-				}
-			}	
-		}				
+		String title;
+		String[] anchorLinks;
+		WebElement anchor;
+		for (Map.Entry<String, String[]> entry : anchorsLinks.entrySet()) {
+			title = entry.getKey();
+			driver.get(URL + title);
+			anchorLinks = entry.getValue();			
+			for(String anchorLink : anchorLinks) {
+				anchor = driver.findElementByLinkText(anchorLink);
+				AssertJUnit.assertNotNull(anchor);
+			}			
+		}		
 	}
 	
 	@Test(enabled = true)
@@ -66,6 +114,19 @@ public class Tests {
 			WebElement img = driver.findElement(By.xpath("//img[contains(@alt,'logo')]"));
 			AssertJUnit.assertNotNull(img);	
 		}		
+	}
+	
+	@Test(enabled = true)
+	public static void pagesShouldHaveForms() {
+		String title, formName;
+		WebElement frm;
+		for (Map.Entry<String, String> entry : pagesWithForms.entrySet()) {
+			title = entry.getKey();
+			formName = entry.getValue();
+			driver.get(URL + title);
+			frm =  driver.findElement(By.xpath("//form[contains(@name, " + formName + ")]"));
+			AssertJUnit.assertNotNull(frm);	
+		}
 	}
 }
 
