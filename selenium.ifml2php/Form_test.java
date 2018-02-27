5d9108f7f3fdd9fe3d0b190435d06dbf944d1f8f
dipiert <dipiert@gmail.com>
2018-02-27 11:53:36 -0300

    Allowing parallel tests on multiples browsers

--- selenium.ifml2php/Form_test.java
+++ selenium.ifml2php/Form_test.java
@@ -1,9 +1,14 @@
 package selenium.ifml2php;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.AssertJUnit;
 import org.testng.annotations.AfterClass;
@@ -12,14 +17,15 @@ import org.testng.annotations.Test;
 
 public class Form_test {
 	
-	private static FirefoxDriver driver;
 	private static String lvlBaseDirController, yiiBaseDirController;	
 	private static Yii yii;
 	private static Laravel laravel;
+	private static List<WebDriver> drivers;
 	
 	public Form_test() {
 		String server = "localhost";
 		String controller = "movie";
+		drivers = new ArrayList<WebDriver>();
 		yii = new Yii(server, controller);
 		yiiBaseDirController = yii.getBaseDirController();
 		laravel = new Laravel(server, controller);
@@ -27,63 +33,89 @@ public class Form_test {
 	}
 	
 	@BeforeClass
-	private void setUp() {
-		System.setProperty("webdriver.gecko.driver", "geckodriver");
-		driver = new FirefoxDriver();
+	private void setUp() {		
+		registerDrivers();
+		loadDrivers();
+	}
+	
+	private void loadFirefoxDriver() {
+		drivers.add(new FirefoxDriver());
+	}
+	
+	private void loadChromeDriver() {
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.addArguments("--no-sandbox");
+		drivers.add(new ChromeDriver(chromeOptions));
+	}
+	private void loadDrivers() {
+		loadFirefoxDriver();
+		loadChromeDriver();		
+	}
+	
+	private void registerDrivers() {
+		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+		System.setProperty("webdriver.gecko.driver", "drivers/geckodriver");
 	}
 	
 	@AfterClass
 	private static void tearDown() {
-		driver.close();
+		for (WebDriver driver : drivers)
+			driver.close();
 	}
 	
 	private static void formsShouldHaveTextInputs(String baseDirController, Map<String, String[]> inputTexts){
 		String name, query;
 		String[] inputsTextsName;
 		WebElement inputText;	
-		for (Map.Entry<String, String[]> entry : inputTexts.entrySet()) {
-			name = entry.getKey();
-			driver.get(baseDirController + name);
-			inputsTextsName = entry.getValue();
-			for(String inputTextName : inputsTextsName) {
-				query = "//input[contains(@name," + inputTextName + ")]";
-				inputText = driver.findElement(By.xpath(query));				
-				AssertJUnit.assertNotNull(inputText);
-				AssertJUnit.assertNotNull(inputText.getAttribute("type").equals("text"));				
-			}
-		}		
+		for (WebDriver driver : drivers){
+			for (Map.Entry<String, String[]> entry : inputTexts.entrySet()) {
+				name = entry.getKey();
+				driver.get(baseDirController + name);
+				inputsTextsName = entry.getValue();
+				for(String inputTextName : inputsTextsName) {
+					query = "//input[contains(@name," + inputTextName + ")]";
+					inputText = driver.findElement(By.xpath(query));				
+					AssertJUnit.assertNotNull(inputText);
+					AssertJUnit.assertNotNull(inputText.getAttribute("type").equals("text"));				
+				}
+			}	
+		}				
 	}
 	
 	private static void formsShouldHaveLabels(String baseDirController, Map<String, String[]> labelsPage) {
 		String name, query;
 		String[] labelsName;
 		WebElement label;
-		for (Map.Entry<String, String[]> entry : labelsPage.entrySet()) {
-			name = entry.getKey();
-			driver.get(baseDirController + name);
-			labelsName = entry.getValue();
-			for (String labelName : labelsName) {
-				query = "//label[contains(@for," + labelName + " )]";
-				label = driver.findElement(By.xpath(query));
-				AssertJUnit.assertNotNull(label);
-			}
-		}
+		for (WebDriver driver : drivers){
+			for (Map.Entry<String, String[]> entry : labelsPage.entrySet()) {
+				name = entry.getKey();
+				driver.get(baseDirController + name);
+				labelsName = entry.getValue();
+				for (String labelName : labelsName) {
+					query = "//label[contains(@for," + labelName + " )]";
+					label = driver.findElement(By.xpath(query));
+					AssertJUnit.assertNotNull(label);
+				}
+			}	
+		}		
 	}
 
 	private static void formsShouldHaveRadioButtons(String baseDirController, Map<String, String[]> radiosPage) {
 		String name;
 		String[] radioNames;
 		WebElement radio;
-		for (Map.Entry<String, String[]> entry : radiosPage.entrySet()) {
-			name = entry.getKey();
-			driver.get(baseDirController + name);
-			radioNames = entry.getValue();
-			for(String radioName : radioNames) {
-				radio = driver.findElement(By.xpath("//input[contains(@name," + radioName + ")]"));
-				AssertJUnit.assertNotNull(radio);
-				AssertJUnit.assertNotNull(radio.getAttribute("type").equals("radio"));
-			}
-		}
+		for(WebDriver driver : drivers) {
+			for (Map.Entry<String, String[]> entry : radiosPage.entrySet()) {
+				name = entry.getKey();
+				driver.get(baseDirController + name);
+				radioNames = entry.getValue();
+				for(String radioName : radioNames) {
+					radio = driver.findElement(By.xpath("//input[contains(@name," + radioName + ")]"));
+					AssertJUnit.assertNotNull(radio);
+					AssertJUnit.assertNotNull(radio.getAttribute("type").equals("radio"));
+				}
+			}	
+		}		
 	}
 
 	@Test(enabled = true)
--- selenium.ifml2php/Page_test.java
+++ selenium.ifml2php/Page_test.java
@@ -1,5 +1,6 @@
 package selenium.ifml2php;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -8,13 +9,17 @@ import org.testng.annotations.Test;
 import org.testng.Assert;
 import org.testng.AssertJUnit;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 
 public class Page_test {
     
-    private static FirefoxDriver driver;
+    private static List<WebDriver> drivers;
     private static String yiiBaseDirController, lvlBaseDirController;    
     private static Yii yii;
     private static Laravel laravel;
@@ -22,6 +27,7 @@ public class Page_test {
     public Page_test() {    
     	String server = "localhost";
 		String controller = "movie";
+		drivers = new ArrayList<WebDriver>();
     	yii = new Yii(server, controller);
         laravel = new Laravel(server, controller);
     	yiiBaseDirController = yii.getBaseDirController();
@@ -30,64 +36,93 @@ public class Page_test {
     
     @BeforeClass
     private static void setUp() {
-        System.setProperty("webdriver.gecko.driver", "geckodriver");
-        driver = new FirefoxDriver();
+    	registerDrivers();
+    	loadDrivers();
+    }
+    
+    private static void registerDrivers() {
+		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+		System.setProperty("webdriver.gecko.driver", "drivers/geckodriver");
+    }
+    
+    private static void loadDrivers() {
+    	loadFirefoxDriver();
+		loadChromeDriver();	
+    }
+    
+    private static void loadFirefoxDriver() {
+    	drivers.add(new FirefoxDriver());
+    }
+    
+    private static void loadChromeDriver() {
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.addArguments("--no-sandbox");
+		drivers.add(new ChromeDriver(chromeOptions));
     }
     
     @AfterClass
     private static void tearDown() {
-        driver.close();
+    	for (WebDriver driver : drivers)
+    		driver.close();
     }
        
     private static void pagesShouldHaveCorrectTitle(String baseDirController, Map<String, String> pagesTitles) {
         String page, title;
-        for (Map.Entry<String, String> entry : pagesTitles.entrySet()) {
-            page = entry.getKey();
-            title = entry.getValue();
-            driver.get(baseDirController + page);
-            Assert.assertEquals(driver.getTitle(),title);
-        }
+        for(WebDriver driver : drivers) {
+        	for (Map.Entry<String, String> entry : pagesTitles.entrySet()) {
+                page = entry.getKey();
+                title = entry.getValue();
+                driver.get(baseDirController + page);
+                Assert.assertEquals(driver.getTitle(),title);
+            }	
+        }        
     }   
 
     private static void pagesShouldHaveFormsWithSubmit(String baseDirController, Map<String, String> pagesWithForms) {
         String title, formName, query;
         List<WebElement> allFormChildElements;
-        for (Map.Entry<String, String> entry : pagesWithForms.entrySet()) {
-            title = entry.getKey();
-            formName = entry.getValue();
-            driver.get(baseDirController + title);
-            query = "//form[contains(@name, " + formName + ")]";
-            allFormChildElements =  driver.findElements(By.xpath(query));
-            AssertJUnit.assertNotNull(allFormChildElements);
-            for(WebElement item : allFormChildElements)
-                if(item.getTagName().equals("input") && item.getAttribute("type").equals("submit"))
-                    AssertJUnit.assertNotNull(item);            
-        }
+        for (WebDriver driver : drivers) {
+        	for (Map.Entry<String, String> entry : pagesWithForms.entrySet()) {
+                title = entry.getKey();
+                formName = entry.getValue();
+                driver.get(baseDirController + title);
+                query = "//form[contains(@name, " + formName + ")]";
+                allFormChildElements =  driver.findElements(By.xpath(query));
+                AssertJUnit.assertNotNull(allFormChildElements);
+                for(WebElement item : allFormChildElements)
+                    if(item.getTagName().equals("input") && item.getAttribute("type").equals("submit"))
+                        AssertJUnit.assertNotNull(item);            
+            }	
+        }        
     }
     
     private static void pagesShouldHaveAnchors(String baseDirController, Map<String, String[]> anchorsLinks) {
         String title;
         String[] anchorLinks;
         WebElement anchor;
-        for (Map.Entry<String, String[]> entry : anchorsLinks.entrySet()) {
-            title = entry.getKey();
-            driver.get(baseDirController + title);
-            anchorLinks = entry.getValue();         
-            for(String anchorLink : anchorLinks) {
-                anchor = driver.findElementByLinkText(anchorLink);
-                AssertJUnit.assertNotNull(anchor);
-            }           
-        }       
+        for (WebDriver driver : drivers) {
+        	for (Map.Entry<String, String[]> entry : anchorsLinks.entrySet()) {
+                title = entry.getKey();
+                driver.get(baseDirController + title);
+                anchorLinks = entry.getValue();         
+                for(String anchorLink : anchorLinks) {
+                    anchor = ((RemoteWebDriver) driver).findElementByLinkText(anchorLink);
+                    AssertJUnit.assertNotNull(anchor);
+                }           
+            }	
+        }               
     }
     
     private static void pagesShouldDisplayImages(String baseDirController, String[] pagesWithImages) {
         String query;
-        for(String pageWithImage : pagesWithImages) {
-            driver.get(baseDirController + pageWithImage);  
-            query = "//img[contains(@alt,'logo')]";
-            WebElement img = driver.findElement(By.xpath(query));
-            AssertJUnit.assertNotNull(img); 
-        }       
+        for (WebDriver driver : drivers) {
+        	for(String pageWithImage : pagesWithImages) {
+                driver.get(baseDirController + pageWithImage);  
+                query = "//img[contains(@alt,'logo')]";
+                WebElement img = driver.findElement(By.xpath(query));
+                AssertJUnit.assertNotNull(img); 
+            }	
+        }               
     }   
     
     @Test(enabled = true)
