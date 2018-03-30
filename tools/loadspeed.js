a3ff3141bbd90062acc6e711888a4483146297d1
Damián Rotta <damian_rott2@hotmail.com>
2018-03-30 08:59:54 -0300

    Add Yii2 routes to loadspeed test

--- tools/loadspeed.js
+++ tools/loadspeed.js
@@ -1,37 +1,45 @@
 var urls = getLaravelURLs();
+Array.prototype.push.apply(urls, getYiiURLs());
 processPages();
 
+
 function getLaravelURLs() {
-	var home = 'http://localhost/laravel5.4.15/public/'
-	var routes = ['movie/MainMenuMovie',
-		'movie/AddFormMovie',
-		'movie/UpdateFormMovie',
-		'movie/DeleteFormMovie',
-		'movie',
-		'movie/addMovie',
-		'movie/updateMovie',
-		'movie/deleteMovie'];
-	return routes.map(function(r) {
-		return home + r;
-	});
+    var home = 'http://localhost/laravel5.4.15/public/'
+    var routes = ['movie/MainMenuMovie',
+        'movie/AddFormMovie',
+        'movie/UpdateFormMovie',
+        'movie/DeleteFormMovie',
+    ];
+    return makeURL(home, routes);
 }
 
+function getYiiURLs() {
+    var home = 'http://localhost/yii2.0.12/views/InteractionFlowModelViews';
+    var routes = [ 'MainMenuMovie', 'AddFormMovie', 'UpdateFormMovie', 'DeleteFormMovie'];
+    return makeURL(home, routes);
+}
+
+function makeURL(home, routes) {
+    return routes.map(function(r) {
+        return home + r;
+    });
+}
 function processPages() {
-	if (urls.length == 0) {
-		phantom.exit();
-	} else {
-		url = urls.shift();
-		console.log("Loading " + url);
-		t = Date.now();
-		page = require('webpage').create();
-		page.open(url, onFinishedLoading);		
-	}
+    if (urls.length == 0) {
+        phantom.exit();
+    } else {
+        url = urls.shift();
+        console.log("Loading " + url);
+        t = Date.now();
+        page = require('webpage').create();
+        page.open(url, onFinishedLoading);      
+    }
 }
 
 function onFinishedLoading(status) {
-	t = Date.now() - t;
-	console.log(status);
-	console.log('Loading time ' + t + ' msec\n');
-	page.release();
-	processPages();
+    t = Date.now() - t;
+    console.log(status);
+    console.log('Loading time ' + t + ' msec\n');
+    page.release();
+    processPages();
 }
